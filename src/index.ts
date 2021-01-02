0ccf305b0a5695f10e027c59ee88ceca434c975d
mtannaan <12782884+mtannaan@users.noreply.github.com>
2021-01-02 18:43:39 +0900

    Format index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import * as express from "express";
-import { env } from "process";
+import * as express from 'express';
+import {env} from 'process';
 const PORT = env.PORT || 3000;
 
 const app = express();
-app.get('/ping', (req, res) => { res.send('pong'); });
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
 console.log(`Listening to port ${PORT}...`);
 app.listen(PORT);
