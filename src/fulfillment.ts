ca18dea96766c744978307aad73ca9588ee5a948
mtannaan <12782884+mtannaan@users.noreply.github.com>
2021-01-02 22:03:24 +0900

    Add auth provider skeleton

--- src/fulfillment.ts
+++ src/fulfillment.ts
@@ -1,3 +1,3 @@
 import {smarthome} from 'actions-on-google';
 
-export const smartHomeApp = smarthome();
+export const app = smarthome();
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import {env} from 'process';
 
 import * as express from 'express';
 
-import {smartHomeApp} from './fulfillment';
+import {app as authProviderApp} from './auth-provider';
+import {app as smartHomeApp} from './fulfillment';
 
 const PORT = env.PORT || 3000;
 
 const app = express();
 app.use('/fulfillment', express.json());
+app.use('/auth', authProviderApp);
 
 app.get('/ping', (req, res) => {
   res.send('pong');
