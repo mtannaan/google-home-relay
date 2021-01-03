46b59ca73b2e07438ffc24e6d6ff44568aced799
mtannaan <12782884+mtannaan@users.noreply.github.com>
2021-01-03 12:01:31 +0900

    Add SYNC request handling

--- src/fulfillment.ts
+++ src/fulfillment.ts
@@ -1,8 +1,23 @@
 import {smarthome} from 'actions-on-google';
 
+import {devices} from './devices';
+
+const agentUserId = 'user9999';
+
 const jwt =
   typeof process.env.TOKEN_CREATOR_JWT === 'string'
     ? JSON.parse(Buffer.from(process.env.TOKEN_CREATOR_JWT, 'hex').toString())
     : null;
 
 export const app = smarthome({jwt});
+
+app.onSync(body => {
+  // TODO: get user ID from headers
+  return {
+    requestId: body.requestId,
+    payload: {
+      agentUserId,
+      devices,
+    },
+  };
+});
